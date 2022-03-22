4bc218358aef9cf2fba39a60813d0bfd9f412744
JuliusMushi19 <48461409+JuliusMushi19@users.noreply.github.com>
2022-03-21 21:23:28 -0400

    Add files via upload

---
+++ Registration.aspx
@@ -0,0 +1,95 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ALZ/MasterPage.Master" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="ALZ.ALZ.Registration" %>
+
+<asp:Content ID="MyContent" ContentPlaceHolderID="MyContentPlaceHolder" runat="server">
+    <table width="772" cellspacing="0" cellpadding="0" border="0" align="center">
+    <tr>
+        <td>
+
+            <table border="0">
+                <tr>
+                    <td colspan="5"><h2>Registration Form</h2></td>
+                </tr>
+                <tr>
+                    <td width="10"><img src="images/required.gif" /></td>
+                    <td width="5">&nbsp;</td>
+                    <td>First Name:</td>
+                    <td width="5">&nbsp;</td>
+                    <td><asp:TextBox ID="txtFirstName" runat="server" Columns="35" MaxLength="50"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td width="10"><img src="images/required.gif" /></td>
+                    <td width="5">&nbsp;</td>
+                    <td>Last Name:</td>
+                    <td width="5">&nbsp;</td>
+                    <td><asp:TextBox ID="txtLastName" runat="server" Columns="35" MaxLength="50"></asp:TextBox></td>
+                </tr>
+                   <tr>
+                    <td width="10"><img src="images/required.gif" /></td>
+                    <td width="5">&nbsp;</td>
+                    <td>E-mail Address:</td>
+                    <td width="5">&nbsp;</td>
+                    <td><asp:TextBox ID="txtEmailAddress" runat="server" Columns="35" MaxLength="50"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td width="10">&nbsp;</td>
+                    <td width="5">&nbsp;</td>
+                    <td>Phone:</td>
+                    <td width="5">&nbsp;</td>
+                    <td><asp:TextBox ID="txtPhone" runat="server" Columns="35" MaxLength="50"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td width="10">&nbsp;</td>
+                    <td width="5">&nbsp;</td>
+                    <td>Company Name:</td>
+                    <td width="5">&nbsp;</td>
+                    <td><asp:TextBox ID="txtCompanyName" runat="server" Columns="35" MaxLength="50"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td width="10">&nbsp;</td>
+                    <td width="5">&nbsp;</td>
+                    <td>Job Title:</td>
+                    <td width="5">&nbsp;</td>
+                    <td><asp:TextBox ID="txtJobTitle" runat="server" Columns="35" MaxLength="50"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td colspan="5"><h2>Donations</h2></td>
+                </tr>
+                <tr>
+                    <td colspan="5"><asp:RadioButton ID="txt120" runat="server" Text="$120" GroupName="Dollar" /> <asp:RadioButton ID="txt60" runat="server" Text="$60" GroupName="Dollar" /> <asp:RadioButton ID="txt30" runat="server" Text="$30" GroupName="Dollar" /></td>
+                </tr>
+                <tr>
+                    <td colspan="5"><h2>Gender</h2></td>
+                </tr>
+                 <tr>
+                    <td colspan="5"><asp:RadioButton ID="txtMale" runat="server" Text="Male" GroupName="Gender" /> <asp:RadioButton ID="txtFemale" runat="server" Text="Female" GroupName="Gender" /> <asp:RadioButton ID="txtNoAnswerGender" runat="server" Text="I prefer not to answer" GroupName="Gender" /></td>
+                </tr>
+                 <tr>
+                    <td colspan="5"><h2>Race</h2></td>
+                </tr>
+                 <tr>
+                    <td colspan="5"><asp:RadioButton ID="txtBlack" runat="server" Text="African-American/Black" GroupName="Race" /> <asp:RadioButton ID="txtNative" runat="server" Text="American Indian/Alaskan Native" GroupName="Race" /> <asp:RadioButton ID="txtAsian" runat="server" Text="Asian" GroupName="Race" /> <asp:RadioButton ID="txtWhite" runat="server" Text="Caucasian/White" GroupName="Race" /> <asp:RadioButton ID="txtLatino" runat="server" Text="Hispanic/Latino" GroupName="Race" /> <asp:RadioButton ID="txtIslander" runat="server" Text="Native Hawaiian/Other Pacific Islander" GroupName="Race" /> <asp:RadioButton ID="txtMoreRaces" runat="server" Text="Two or more races" GroupName="Race" /> <asp:RadioButton ID="txtOtherRace" runat="server" Text="Other race" GroupName="Race" /> <asp:RadioButton ID="txtNoAnswerRaces" runat="server" Text="I prefer not to answer" GroupName="Race" /></td>
+                </tr>
+                   <tr>
+                    <td colspan="5"><h2>Education</h2></td>
+                </tr>
+                 <tr>
+                    <td colspan="5"><asp:RadioButton ID="txtLessHSDegree" runat="server" Text="Less than high school degree" GroupName="Degree" /> <asp:RadioButton ID="txtHSGraduate" runat="server" Text="High school graduate" GroupName="Degree" /> <asp:RadioButton ID="txtSomeCollege" runat="server" Text="Some college" GroupName="Degree" /> <asp:RadioButton ID="txtBachelor" runat="server" Text="Bachelor degree" GroupName="Degree" /> <asp:RadioButton ID="txtProfessional" runat="server" Text="Post/Professional degree" GroupName="Degree" /> <asp:RadioButton ID="txtNoAnswerEducation" runat="server" Text="I prefer not to answer" GroupName="Degree" /></td>
+                </tr>
+                   <tr>
+                    <td colspan="5"><h2>T-shirt Size</h2></td>
+                </tr>
+                <tr>
+                    <td colspan="5"><asp:RadioButton ID="txtSmall" runat="server" Text="Small" GroupName="TShirt" /> <asp:RadioButton ID="txtLarge" runat="server" Text="Large" GroupName="TShirt" /> <asp:RadioButton ID="txtXLarge" runat="server" Text="X-Large" GroupName="TShirt" /> <asp:RadioButton ID="txtXXLarge" runat="server" Text="XX-Large" GroupName="TShirt" /></td>
+                </tr>
+                <tr>
+                    <td colspan="5">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td colspan="5"><asp:Button ID="cmdSubmit" runat="server" Text="Submit Registration" OnClick="cmdSubmit_Click" />&nbsp;&nbsp;&nbsp;<asp:LinkButton ID="cmdCancel" runat="server" OnClick="cmdCancel_Click">Cancel</asp:LinkButton></td>
+                </tr>
+            </table>
+
+        </td>
+    </tr>
+</table>
+</asp:Content>
---
+++ Registration.aspx.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ALZ.ALZ
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtFirstName.Focus();
+            }
+        }
+
+        private void ShowMessage(String MessageToDisplay)
+        {
+            // define the name and type of the client script
+            String csName = "PopupScript";
+            Type csType = this.GetType();
+
+            // get a ClientScriptManager object
+            System.Web.UI.ClientScriptManager cs = Page.ClientScript;
+
+            // check to see if the startup script is already registered
+            if (!cs.IsStartupScriptRegistered(csType, csName))
+            {
+                String csText = "alert('" + MessageToDisplay + "');";
+                cs.RegisterStartupScript(csType, csName, csText, true);
+            }
+        }
+
+        private Boolean VerifyForm()
+        {
+            if (txtFirstName.Text == "")
+            {
+                ShowMessage("Please provide your first name.");
+                txtFirstName.Focus();
+                return false;
+            }
+            else if (txtLastName.Text == String.Empty)
+            {
+                ShowMessage("Please provide your last name.");
+                txtLastName.Focus();
+                return false;
+            }
+            else if (txtEmailAddress.Text == String.Empty)
+            {
+                ShowMessage("Please provide your e-mail address.");
+                txtEmailAddress.Focus();
+                return false;
+            }
+            else if (txt120.Checked == false && txt60.Checked == false && txt30.Checked == false)
+            {
+                ShowMessage("Please select a donation.");
+                txt120.Focus();
+                return false;
+            }
+            else if (txtMale.Checked == false && txtFemale.Checked == false && txtNoAnswerGender.Checked == false)
+            {
+                ShowMessage("Please select a gender.");
+                txtMale.Focus();
+                return false;
+            }
+            else if (txtBlack.Checked == false && txtNative.Checked == false && txtAsian.Checked == false && txtWhite.Checked == false && txtLatino.Checked == false && txtIslander.Checked == false && txtMoreRaces.Checked == false && txtOtherRace.Checked == false && txtNoAnswerRaces.Checked == false)
+            {
+                ShowMessage("Please select a race.");
+                txtBlack.Focus();
+                return false;
+            }
+            else if (txtLessHSDegree.Checked == false && txtHSGraduate.Checked == false && txtSomeCollege.Checked == false && txtBachelor.Checked == false && txtProfessional.Checked == false)
+            {
+                ShowMessage("Please select an education.");
+                txtLessHSDegree.Focus();
+                return false;
+            }
+            else if (txtSmall.Checked == false && txtLarge.Checked == false && txtXLarge.Checked == false & txtXXLarge.Checked == false)
+            {
+                ShowMessage("Please select a t-shirt size.");
+                txtSmall.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        protected Boolean AddRegistration()
+        {
+            try
+            {
+                // setup and create the connection to the database
+                ADODB.Connection objConnect = new ADODB.Connection();
+                ADODB.Recordset objRS = new ADODB.Recordset();
+
+                String strConnection = "Data Source=csis-sql.elmhurst.edu;Initial Catalog=MIT543-ERM-Symposium-Mushi;User ID=cs550;Password=Elmhurst1871;Provider=msoledbsql;";
+                String SQL = "SELECT * FROM [ALZ_Registrations];";
+
+                // open the connection
+                objConnect.Open(strConnection);
+                objRS.Open(SQL, objConnect, ADODB.CursorTypeEnum.adOpenStatic, ADODB.LockTypeEnum.adLockOptimistic);
+
+                // add registration record
+                objRS.AddNew();
+
+                objRS.Fields["RegistrationID"].Value = Guid.NewGuid().ToString();
+
+                objRS.Fields["FirstName"].Value = txtFirstName.Text;
+                objRS.Fields["LastName"].Value = txtLastName.Text;
+                objRS.Fields["EmailAddress"].Value = txtEmailAddress.Text;
+
+
+                if (txtPhone.Text != String.Empty)
+                {
+                    objRS.Fields["Phone"].Value = txtPhone.Text;
+                }
+                else
+                {
+                    objRS.Fields["Phone"].Value = String.Empty;
+                }
+
+                if (txtCompanyName.Text != String.Empty)
+                {
+                    objRS.Fields["CompanyName"].Value = txtCompanyName.Text;
+                }
+                else
+                {
+                    objRS.Fields["CompanyName"].Value = String.Empty;
+                }
+
+                if (txtJobTitle.Text != String.Empty)
+                {
+                    objRS.Fields["JobTitle"].Value = txtJobTitle.Text;
+                }
+                else
+                {
+                    objRS.Fields["JobTitle"].Value = String.Empty;
+                }
+
+                if (txt120.Checked == true)
+                {
+                    objRS.Fields["Donation"].Value = "$120";
+                }
+                else if (txt60.Checked == true)
+                {
+                    objRS.Fields["Donation"].Value = "$60";
+                }
+                else if (txt30.Checked == true)
+                {
+                    objRS.Fields["Donation"].Value = "$30";
+                }
+
+                if (txtMale.Checked == true)
+                {
+                    objRS.Fields["Gender"].Value = "Male";
+                }
+                else if (txtFemale.Checked == true)
+                {
+                    objRS.Fields["Gender"].Value = "Female";
+                }
+                else if (txtNoAnswerGender.Checked == true)
+                {
+                    objRS.Fields["Gender"].Value = "I prefer not to answer";
+                }
+
+
+                if (txtBlack.Checked == true)
+                {
+                    objRS.Fields["Race"].Value = "African-American/Black";
+                }
+                else if (txtNative.Checked == true)
+                {
+                    objRS.Fields["Race"].Value = "American Indian/Alaskan Native";
+                }
+                else if (txtAsian.Checked == true)
+                {
+                    objRS.Fields["Race"].Value = "Asian";
+                }
+                else if (txtWhite.Checked == true)
+                {
+                    objRS.Fields["Race"].Value = "Caucasian/White";
+                }
+                else if (txtLatino.Checked == true)
+                {
+                    objRS.Fields["Race"].Value = "Hispanic/Latino";
+                }
+                else if (txtIslander.Checked == true)
+                {
+                    objRS.Fields["Race"].Value = "Native Hawaiian/Other Pacific Islander";
+                }
+                else if (txtMoreRaces.Checked == true)
+                {
+                    objRS.Fields["Race"].Value = "Two or more races";
+                }
+                else if (txtNoAnswerRaces.Checked == true)
+                {
+                    objRS.Fields["Race"].Value = "I prefer not to answer";
+                }
+
+                if (txtLessHSDegree.Checked == true)
+                {
+                    objRS.Fields["Education"].Value = "Less than high school degree";
+                }
+                else if (txtHSGraduate.Checked == true)
+                {
+                    objRS.Fields["Education"].Value = "High school graduate";
+                }
+                else if (txtSomeCollege.Checked == true)
+                {
+                    objRS.Fields["Education"].Value = "Some college";
+                }
+                else if (txtBachelor.Checked == true)
+                {
+                    objRS.Fields["Education"].Value = "Bachelor degree";
+                }
+                else if (txtProfessional.Checked == true)
+                {
+                    objRS.Fields["Education"].Value = "Post/Professional";
+                }
+                else if (txtNoAnswerEducation.Checked == true)
+                {
+                    objRS.Fields["Education"].Value = "I prefer not to answer";
+                }
+
+
+                if (txtSmall.Checked == true)
+                {
+                    objRS.Fields["TshirtSize"].Value = "Small";
+                }
+                else if (txtLarge.Checked == true)
+                {
+                    objRS.Fields["TshirtSize"].Value = "Large";
+                }
+                else if (txtXLarge.Checked == true)
+                {
+                    objRS.Fields["TshirtSize"].Value = "X-Large";
+                }
+                else if (txtXXLarge.Checked == true)
+                {
+                    objRS.Fields["TshirtSize"].Value = "XX-Large";
+                }
+
+                objRS.Fields["IsDeleted"].Value = 0;
+
+
+                // create a datetime stamp
+                objRS.Fields["DateTimeCreated"].Value = DateTime.Now;
+
+                // save the record to the database
+                objRS.Update();
+
+                // close the connection to the database
+                objRS.Close();
+                objConnect.Close();
+
+                // destroy the objects
+                objRS = null;
+                objConnect = null;
+
+                return true;
+            }
+            catch (Exception)
+            {
+                // throw;
+                return false;
+            }
+        }
+
+
+        protected void cmdSubmit_Click(object sender, EventArgs e)
+        {
+            if (VerifyForm() == true)
+            {
+                    if (AddRegistration() == true)
+                    {
+                        // redirect to the thank you page
+                        Response.Redirect("ThankYou.aspx");
+                    }
+                    else
+                    {
+                        // redirect to the error page
+                        Response.Redirect("Error.aspx");
+                    }
+               
+            }
+
+        }
+
+        protected void cmdCancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("https://act.alz.org/");
+        }
+    }
+}
